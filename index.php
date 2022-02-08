da018b76f9434155d9a664c1f22019702c00c7cb
GD1920074 <98942291+GD1920074@users.noreply.github.com>
2022-02-08 03:02:53 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ TLE Scaffold
 
 <marquee direction="up" scroll amount="30">
  
- <h2 align="center"><font size="15" face="Times New Roman" Color="#000C66"> HOME PAGE </font> </h2>
+ <h2 align="center"><font size="15" face="Times New Roman" Color="#821D30"> HOME PAGE </font> </h2>
   
 </marquee>
 
