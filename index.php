ea6cf720b0316df46b776c79b0fa348fc67275d2
GD1920074 <98942291+GD1920074@users.noreply.github.com>
2022-02-08 01:22:15 -0500

    Update index.php

--- index.php
+++ index.php
@@ -15,11 +15,11 @@ TLE Scaffold
 <td><aa href="CAE.html">Click for the link</a></td>
 </tr>
 <tr align="center">
-<td><font size="12" face="Oswald">CAE</font></td>
+<td><font size="12" face="Oswald">CLE</font></td>
 <td><aa href="CLE.html">Click for the link</a></td>
 </tr>
 <tr align="center">
-<td><font size="12" face="Oswald">CAE</font></td>
+<td><font size="12" face="Oswald">MATH</font></td>
 <td><aa href="MATH.html">Click for the link</a></td>
 </tr>
 </table>
